42e896919f0951e82b4ed7d8697c9b237cf13253
Hrustios <andrejkulikov555@gmail.com>
2026-04-20 09:49:46 +0300

    delete all

--- Backend/Backend/extensions.py
+++
@@ -1,6 +0,0 @@
-from flask_mail import Mail
-from itsdangerous import URLSafeTimedSerializer
-
-mail = Mail()
-# Этот секретный ключ должен быть таким же, как в app.secret_key
-serializer = URLSafeTimedSerializer("chichiwichki")
